caa8956a30f92684bef62a41f62aef89d7a2d543
jdmcpeek <david.mcpeek@yale.edu>
2014-07-21 16:02:27 -0400

    figuring out index v. show for auditions

--- YFA/app/controllers/auditions_controller.rb
+++ YFA/app/controllers/auditions_controller.rb
@@ -1,11 +1,28 @@
 class AuditionsController < ApplicationController
   def new
-  	@audition = Audition.new
+  	@project = Project.find(params[:project])
+  	@audition = Audition.new(project_id: params[:project])
   end
 
   def create
-  	
+  	@audition = Audition.create(audition_params)
+
+ #  	@project = Project.find_by(id: params[:project_id])
+	# @project.auditions.create(audition_params)
+	redirect_to audition_path(@audition)
   end
+
+  def show
+
+  	@audition
+
+  end
+
+  def index
+  	
+
+  end
+
 
   def edit
   end
