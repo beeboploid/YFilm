541b08b881b6690571af2e36f70b3b2674cdbd7f
jdmcpeek <david.mcpeek@yale.edu>
2014-07-21 14:01:00 -0400

    adding datepicker jquery

--- YFA/app/controllers/auditions_controller.rb
+++ YFA/app/controllers/auditions_controller.rb
@@ -1,8 +1,10 @@
 class AuditionsController < ApplicationController
   def new
+  	@audition = Audition.new
   end
 
   def create
+  	
   end
 
   def edit
@@ -10,4 +12,13 @@ class AuditionsController < ApplicationController
 
   def update
   end
+
+  private
+
+  	def audition_params
+  		params.require(:audition).permit(:project_id, :user_id, :user_name, :when)
+  	end
+
+
+
 end
