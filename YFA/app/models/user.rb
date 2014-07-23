19d87eaff47e004ca97691548c2e9f9018f04d7b
jdmcpeek <david.mcpeek@yale.edu>
2014-07-22 22:39:06 -0400

    Make a basic User model (including secure passwords)

--- YFA/app/models/user.rb
+++ YFA/app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 } #these are simply methods
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[yale]+\.[edu]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-
-
-
-   
-  
+  validates :email, presence: true, 
+            format: { with: VALID_EMAIL_REGEX }, 
+            uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+ 
   # Associations
   has_many :roles
   has_many :projects, through: :roles
--- YFA/db/schema.rb
+++ YFA/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140723015944) do
+ActiveRecord::Schema.define(version: 20140723023331) do
 
   create_table "auditions", force: true do |t|
     t.integer  "project_id"
@@ -67,6 +67,9 @@ ActiveRecord::Schema.define(version: 20140723015944) do
     t.string   "college"
     t.text     "bio"
     t.string   "name"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
