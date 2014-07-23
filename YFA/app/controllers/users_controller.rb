7012bb91f89114109b6fdc739ba7a464230beb46
jdmcpeek <david.mcpeek@yale.edu>
2014-07-22 22:04:48 -0400

    migrate new name attribute and remove netid column

--- YFA/app/controllers/users_controller.rb
+++ YFA/app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   end
 
   def new #will need landing page
+    @user = User.new
 
   end
 
--- YFA/db/schema.rb
+++ YFA/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722140754) do
+ActiveRecord::Schema.define(version: 20140723015944) do
 
   create_table "auditions", force: true do |t|
     t.integer  "project_id"
@@ -60,13 +60,13 @@ ActiveRecord::Schema.define(version: 20140722140754) do
   create_table "users", force: true do |t|
     t.string   "fname"
     t.string   "lname"
-    t.string   "netid"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "year"
     t.string   "college"
     t.text     "bio"
+    t.string   "name"
   end
 
 end
